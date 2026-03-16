941fe6a898eb92d55444e8a45cce99393aabcc4a
SkyeAv <skye.lane.goetz@gmail.com>
2026-03-16 12:53:29 -0700

    Added ClassData mutex

--- cmd/build.go
+++ cmd/build.go
@@ -1,6 +1,21 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"sync"
+
+	"github.com/spf13/cobra"
+)
+
+type ClassData struct {
+	mu   sync.Mutex
+	data map[string][]string
+}
+
+func (cd *ClassData) Set(key string, value []string) {
+	cd.mu.Lock()
+	cd.data[key] = value
+	cd.mu.Unlock()
+}
 
 func build(cmd *cobra.Command, args []string) {
 	synonymDir := args[0]
